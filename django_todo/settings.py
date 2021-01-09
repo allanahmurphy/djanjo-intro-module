613d95413492081dd08986f6e3424988ccf17bf1
allanahmurphy <allanahtmurphy@gmail.com>
2021-01-09 19:10:12 +0000

    Added secret to local environvariables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY','v5t4pvmonj^55fl0ucnnqkaw@hvm%831fqptcibs0ckcgafqix')
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
